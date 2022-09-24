c1b193b92942f18d6b9d1b0c6274562e8cc6ba0b
feklipe tavera <jftaa@hotmail.com>
2022-09-24 12:21:24 -0500

    [ADD] supplies model with migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_24_163857) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_24_171849) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_24_163857) do
     t.integer "price", default: 0, null: false
     t.integer "number_of_guests", default: 0, null: false
     t.integer "number_of_tents", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "supplies", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
