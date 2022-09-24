f3eeed49c88546ef92364d18c536bc31ce4c522e
duvan <duvanfoz@hotmail.com>
2022-09-24 16:34:50 -0500

    [ADD] seed for the project with camps

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+p 'Deleting records...'
+Camp.delete_all
+
+10.times do
+  camp = Camp.create!(
+    title: Faker::Book.title,
+    content: Faker::Quote.famous_last_words,
+    price: rand(150..999),
+    number_of_guests: rand(2..8),
+    number_of_tents: rand(1..3)
+  )
+  p "#{camp.title} has been added to the DB."
+end
+
+p 'All records were created'
