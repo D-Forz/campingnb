480017b5f1ef248db102d3b6394470e61b269286
duvan <duvanfoz@hotmail.com>
2022-09-24 10:58:40 -0500

    [UPDATE] user model

--- db/migrate/20220924154916_devise_create_users.rb
+++ db/migrate/20220924154916_devise_create_users.rb
@@ -6,6 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       ## Database authenticatable
       t.string :first_name, null: false
       t.string :last_name, null: false
+      t.boolean :owner, null: false, default: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_24_154916) do
   create_table "users", force: :cascade do |t|
     t.string "first_name", null: false
     t.string "last_name", null: false
+    t.boolean "owner", default: false, null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
