bf8b062c6b74a7d33ec29edb4e124e1317ecfbf8
duvan <duvanfoz@hotmail.com>
2022-09-24 14:49:16 -0500

    [ADD] camps controller

---
+++ test/controllers/camps_controller_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+
+class CampsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get camps_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get camps_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get camps_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get camps_create_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get camps_edit_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get camps_update_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get camps_destroy_url
+    assert_response :success
+  end
+end
