2f009d94686ec5d5d0becf44319523af29e06007
feklipe tavera <jftaa@hotmail.com>
2022-09-24 16:27:11 -0500

    [ADD] navbar and footer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2022_09_24_171849) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,13 +24,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_24_171849) do
     t.datetime "updated_at", null: false
   end
 
-
-  create_table "supplies", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-  
   create_table "reviews", force: :cascade do |t|
     t.text "content"
     t.integer "rating"
@@ -41,6 +33,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_24_171849) do
     t.datetime "updated_at", null: false
     t.index ["camp_id"], name: "index_reviews_on_camp_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "supplies", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
