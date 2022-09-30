bf1eb316fe3f7dcf6334380b935d31544812c79c
duvan <duvanfoz@hotmail.com>
2022-09-30 07:51:07 -0500

    [ADD] Validations to camps and bookings, user in navbar

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,12 @@ class Booking < ApplicationRecord
   belongs_to :user
 
   validates :start_date, :end_date, presence: true
+  validates :start_date, :end_date, uniqueness: { scope: :camp_id }, in: :camp_start_date_range
+  validates_inclusion_of :start_date, in: :camp_start_date_range
+
+  private
+
+  def camp_start_date_range
+    (camp.start_date..camp.end_date)
+  end
 end
