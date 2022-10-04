fff928aeb139ea09f16d9dc0e7da49620c1cc630
duvan <duvanfoz@hotmail.com>
2022-10-04 15:34:02 -0500

    [ADD] star-rating controller, rewrite showpage

--- db/migrate/20220925163609_create_reviews.rb
+++ db/migrate/20220925163609_create_reviews.rb
@@ -2,7 +2,7 @@ class CreateReviews < ActiveRecord::Migration[7.0]
   def change
     create_table :reviews do |t|
       t.integer :rating
-      t.text :content
+      t.string :content
       t.references :camp, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
 
